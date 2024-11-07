0fe0693aa1149d2c71960a7285d71bf74c25f438
shahidullah177 <mdshahidsumon177@gmail.com>
2024-11-07 20:39:12 +0600

    Problem-5

---
+++ Problem Solving with Typescript/problem-5.ts
@@ -0,0 +1,17 @@
+//Problem 5:=======>
+/*Write a generic function getProperty that takes an object and a property name as arguments and returns the property value. Add a constraint to ensure the property name exists on the object.*/
+
+const getProperty = <T, K extends keyof T> (givenObject:T, key: K): T[K] => {
+return givenObject[key];
+}
+
+
+//Checking
+const person = {
+    name: "Shahidullah",
+    age: 30,
+    isAdult: true
+}
+console.log(getProperty(person, "age"));
+console.log(getProperty(person, "name"));
+console.log(getProperty(person, "isAdult"));
