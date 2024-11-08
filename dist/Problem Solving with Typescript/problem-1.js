c726ecde12a91ddaf5b544219ce4b5208cfe282c
shahidullah177 <mdshahidsumon177@gmail.com>
2024-11-08 20:14:40 +0600

    dist file delete

--- dist/Problem Solving with Typescript/problem-1.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-//Problem 1:==============>
-//Write a TypeScript function sumArray that takes an array of numbers and returns the sum of all elements in the array.
-function sumArray(numbers) {
-    const sum = numbers.map(num => num).reduce((prev, num) => prev + num, 0);
-    return sum;
-}
-const getSum = sumArray([1, 2, 3, 52]);
-console.log(getSum);
--- dist/Problem Solving with Typescript/problem-8.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-//Problem 8:=====>
-//Create a function validateKeys(obj: T, keys: (keyof T)[]) that takes an object obj and an array of keys keys. The function should return true if all of the specified keys exist in the object; otherwise, it should return false.
-const validateKeys = (obj, keys) => {
-    return keys.every((key) => key in obj);
-};
-const person1 = { name: "Alice", age: 25, email: "alice@example.com" };
-console.log(validateKeys(person1, ["name", "age"]));
